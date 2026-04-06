d0b87e5e39bdb5caa25c3d5410f2590f4816833d
Justin Li <145147591+JustinLi2002@users.noreply.github.com>
2026-04-05 20:38:01 -0400

    Update submit_deepmvp_ppi_n2v.sh

--- slurm/submit_deepmvp_ppi_n2v.sh
+++ slurm/submit_deepmvp_ppi_n2v.sh
@@ -8,21 +8,21 @@
 #SBATCH --gres=gpu:A100:1
 #SBATCH --mem=100G
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=justin.2.li@uconn.edu
-#SBATCH -e deepmvp_ppi2.err
-#SBATCH -o deepmvp_ppi2.log
+#SBATCH --mail-user=
+#SBATCH -e deepmvp_ppi.err
+#SBATCH -o deepmvp_ppi.log
 
 echo Running on host `hostname`
 echo Time is `date`
 echo Directory is `pwd`
 
-source /home/FCAM/juli/HRP/my-venv/bin/activate
+source /home/FCAM/your-driectory/my-venv/bin/activate
 
 echo "=== GPU INFO ==="
 nvidia-smi
 echo "================"
 
-cd /home/FCAM/juli/HRP
+cd /your/work/directory
 python deepmvp_ppi.py
 
 echo "Done at" `date`
